b7b67b50f08ff9bf0413955d51491af5b09b86a7
ebchoi <eunbichoi1@gmail.com>
2022-02-10 04:13:53 +0900

    [Add] Product API with sort method

--- services/index.js
+++ services/index.js
@@ -1,3 +1,4 @@
 const memberService = require('./memberService');
+const productService = require('./productService');
 
-module.exports = { memberService };
+module.exports = { memberService, productService };
